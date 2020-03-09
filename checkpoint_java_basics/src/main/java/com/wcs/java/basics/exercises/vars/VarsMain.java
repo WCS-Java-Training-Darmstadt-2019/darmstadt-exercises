d9f4d56b97009ebac1c9fb800201235e27526022
Thorsten Hofmann <thorsten.hofmann@telekom.de>
2020-03-09 14:36:00 +0100

    solved quests 1-3

--- checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/vars/VarsMain.java
+++ checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/vars/VarsMain.java
@@ -1,10 +1,22 @@
 package com.wcs.java.basics.exercises.vars;
 
-
 public class VarsMain {
-	
-	public static void main(String[] args) {
-		
+
+	public static int calculateAge(int geburtsJahr) {
+		int age = 2020 - geburtsJahr;
+		return age;
 	}
 
+	public static boolean isOld(int age) {
+		if (age <= 30) {
+			return true;
+		} else
+			return false;
+	}
+
+	public static void main(String[] args) {
+		String name = "David";
+		int age = 2020 - 1976;
+		System.out.println(name + " ist " + age + " alt.");
+	}
 }
