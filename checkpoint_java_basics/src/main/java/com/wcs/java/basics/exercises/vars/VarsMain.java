a8e5c290021056d239405439c9dee573b4d5c99c
Thorsten Hofmann <thorsten.hofmann@telekom.de>
2020-03-09 15:22:44 +0100

    beautifications

--- checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/vars/VarsMain.java
+++ checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/vars/VarsMain.java
@@ -2,6 +2,12 @@ package com.wcs.java.basics.exercises.vars;
 
 public class VarsMain {
 
+	public static void main(String[] args) {
+		String name = "David";
+		int age = calculateAge(1976);
+		System.out.println(name + " ist " + age + " alt. Deswegen ist er auch alt: " + isOld(age));
+	}
+
 	public static int calculateAge(int geburtsJahr) {
 		int age = 2020 - geburtsJahr;
 		return age;
@@ -14,9 +20,4 @@ public class VarsMain {
 			return true;
 	}
 
-	public static void main(String[] args) {
-		String name = "David";
-		int age = calculateAge(1976);
-		System.out.println(name + " ist " + age + " alt. Ist er deswegen alt? " + isOld(age));
-	}
 }
