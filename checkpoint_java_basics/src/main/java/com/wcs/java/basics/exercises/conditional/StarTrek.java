3d3bb92715a2a46ef93dc5b1304cc9bf73dbb333
Kadir <abdulkadir.erucu@telekom.de>
2020-03-09 15:05:03 +0100

    Test

--- checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/conditional/StarTrek.java
+++ checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/conditional/StarTrek.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class StarTrek {
-	
+
 	public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(
               new InputStreamReader(System.in));
@@ -13,13 +13,15 @@ public class StarTrek {
         System.out.println("Welchen Teil von Startrek möchten sie schauen? Bitte geben sie die Nr ein.");
         
         String eingabe = reader.readLine();
-        int filmNo = //TODO;
+        int filmNo = Integer.parseInt(eingabe);//TODO;
         
-        
-	}
-	
-	
-        
-        
-
+        if (filmNo % 2 == 0) {
+        	System.out.println("Film Nummer " + filmNo + " ist eine gute Wahl. Viel Spass beim gucken.");       
+        		
+        	} else {
+        		
+        	System.out.println("Film Nummer " + filmNo + " ist keine gute Wahl. Verschwende deine Zeit nicht und guck lieber was anderes.");
+        	}
+	       
+        }
 }
--- checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/vars/VarsMain.java
+++ checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/vars/VarsMain.java
@@ -1,10 +1,35 @@
 package com.wcs.java.basics.exercises.vars;
 
-
 public class VarsMain {
-	
+
 	public static void main(String[] args) {
-		
+		String name = "David";
+		int geburtsJahr = 1916;
+		int age = calculateAge(geburtsJahr);
+		boolean alt = istOld(age);
+
+		System.out.println(name + " ist " + age + " alt.");
+		if (alt == true) {
+			System.out.println(name + " ist alt.");
+		} else {
+			System.out.println(name + " ist jung.");
+		}
+
+	}
+
+	private static int calculateAge(int geburtsJahr) {
+		int alter = 2020 - geburtsJahr;
+
+		return alter;
+
+	}
+
+	private static boolean istOld(int age) {
+		if (age >= 60) {
+			return (true);
+		} else {
+			return (false);
+		}
 	}
 
 }
