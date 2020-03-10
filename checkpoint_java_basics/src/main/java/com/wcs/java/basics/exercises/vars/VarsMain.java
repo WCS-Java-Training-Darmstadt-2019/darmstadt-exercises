ab44281e0fc5fdf8533d4bfd09e2ce6d3bb22633
Christoph Abert <christoph.abert@telekom.de>
2020-03-10 10:53:29 +0100

    Vars Aufgabe gelöst

--- checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/vars/VarsMain.java
+++ checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/vars/VarsMain.java
@@ -4,7 +4,22 @@ package com.wcs.java.basics.exercises.vars;
 public class VarsMain {
 	
 	public static void main(String[] args) {
+		String name = "David";
+		int age;
+		
+		//age = 2020 - 1976;
+		age = calculateAge (1976);
+		
+		System.out.println (name + " ist " + age + " Jahre alt");
+		//System.out.println (name + " ist alt=" + isOld(age) +"!");
+	}
+	
+	public static int calculateAge (int geburtsJahr) {
+		return 2020 - geburtsJahr;
+	}
+	
+	public static boolean isOld (int age) {
+		return age>30;
 		
 	}
-
 }
