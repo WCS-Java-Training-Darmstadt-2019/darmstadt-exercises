56332d7906c8867feb882897b6ce62d0d82592d6
Berthold Voitl <berthold.voitl@telekom.de>
2020-03-10 14:12:02 +0100

    array zum teil gelöst

--- checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/arrays/ArraysExercise.java
+++ checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/arrays/ArraysExercise.java
@@ -1,65 +1,92 @@
 package com.wcs.java.basics.exercises.arrays;
 
-import com.sun.tools.javac.util.ArrayUtils;
+import java.util.Arrays;
 
 
-/**
- * 
- * @author dbe
- *
- */
+
 public class ArraysExercise {
 	
-
-
+		
+public static void main(String[] args) { 
 	
-	public static void main(String[] args) {
 		
-		// die methode createArray soll ein string array mit den angegeben werten anlegen
-		
-		
-		String [] names = new String [10];
-						
-		// iteriere über das array und gebe jeden Wert in der console aus.
-
-		for (String s: names){
+	String[]array = createArray("David", 10);
+	
+	
+	for (String s: array){
 		System.out.println(s);				
 		}
-		
+	}
+
+	char [][]array = createGrid('x', 2, 10);
+	
+//	for (char t: array){
+//		System.out.println(t);				
+//		}
+//	
+//	
+//	System.out.println(grid[i][k]);
+	
+
 
 	
-		// erstelle eine methode die ein 2 dimensional;es array (hier grid genannt) erstellt und jeden wert mit dem übergabe wert füllt.
-		int [][] grid  = new int [2][10];
+public static String [] createArray (String name, int number) {
 		
-		//grid = createGrid(2,10,"x");
-		
-		
+		String [] names = new String [number];
 			
-		// iteriere über jede zelle des grids und gebe spalte und zeile aus sowie den inhalt
-			
+		for (int i = 0; i < names.length; i++){
+		names [i] = name;
+		System.out.println(Arrays.toString(names));
+		
+		}			
+		return names;
+		}
+
+
+public static char [][] createGrid(char b, int row, int column) {
+	
+	
+		char [][] grid = new char [row][column];
+		
 		for (int i=0; i<grid.length; i++) {
 			for(int k=0; k <grid[i].length; k++) {
-				System.out.println(grid[i][k]);
-				
-			}
+			grid [i][k] = b;
+			
 		}
-		
-		// x:0,y=0,value=x
-		// x:1,y=0,value=x
-		// etc.
-		
-		// gebe es als einen string aus
-		// bsp = grid mit 10 spalten und 3 zeilen
-		// ---------------------
-		// |x|x|x|x|x|x|x|x|x|x|
-		// |x|x|x|x|x|x|x|x|x|x|
-		// |x|x|x|x|x|x|x|x|x|x|
-		// ---------------------
-		String gridAsString = gridAsString(grid);
-		System.out.println(gridAsString);
-		
 	}
+		return grid;
 	
-	
+		
+//	// erstelle eine methode die ein 2 dimensional;es array (hier grid genannt) erstellt und jeden wert mit dem übergabe wert füllt.
+
+
+
+//	
+//	//grid = createGrid(2,10,"x");
+//	
+//	
+//		
+//	// iteriere über jede zelle des grids und gebe spalte und zeile aus sowie den inhalt
+//		
+//	
+//	
+//	// x:0,y=0,value=x
+//	// x:1,y=0,value=x
+//	// etc.
+//	
+//	// gebe es als einen string aus
+//	// bsp = grid mit 10 spalten und 3 zeilen
+//	// ---------------------
+//	// |x|x|x|x|x|x|x|x|x|x|
+//	// |x|x|x|x|x|x|x|x|x|x|
+//	// |x|x|x|x|x|x|x|x|x|x|
+//	// ---------------------
+//	String gridAsString = gridAsString(grid);
+//	System.out.println(gridAsString);
+//	
+}
 
 }
+
+
+
