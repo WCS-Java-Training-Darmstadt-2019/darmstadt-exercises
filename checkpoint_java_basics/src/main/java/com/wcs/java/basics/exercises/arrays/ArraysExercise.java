d5105868c232c4980971adc24f75c1cc0ebc5108
Christoph Abert <christoph.abert@telekom.de>
2020-03-10 12:29:12 +0100

    Arrays Aufgabe gelöst

--- checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/arrays/ArraysExercise.java
+++ checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/arrays/ArraysExercise.java
@@ -16,13 +16,17 @@ public class ArraysExercise {
 		// dh. in diesem Beispiel soll ein arry mit 10 element angelegt werden, 
 		// Ergebnis wäre = {"David","David","David","David","David","David","David","David","David","David"}
 		
-		string_array = createArray("David", 10);
+		String[] string_array = createArray("David", 10);
 		
 		// iteriere über das array und gebe jeden Wert in der console aus.
 		
+		for (String a : string_array) {
+			System.out.println (a);
+		}
+		
 		// erstelle eine methode die ein 2 dimensionales array (hier grid genannt) erstellt und jeden wert mit dem übergabe wert füllt.
 		
-		grid = createGrid(2,10,"x");
+		char[][] grid = createGrid(2,10,'x');
 		
 		// iteriere über jede zelle des grids und gebe spalte und zeile aus sowie den inhalt
 		// x:0,y=0,value=x
@@ -36,19 +40,59 @@ public class ArraysExercise {
 		// |x|x|x|x|x|x|x|x|x|x|
 		// |x|x|x|x|x|x|x|x|x|x|
 		// ---------------------
-		String gridAsString = gridAsString(grid):
-		System.out.println(gridAsString);
+		
+		String thisGridAsString = gridAsString(grid);
+		System.out.println(thisGridAsString);
 		
 	}
 	
 	public static String[] createArray(String name, int size) {
 		
-	}
-	
-	public static ??? createGrid(int rows, int columns)  {
+		String[] newArray = new String [size];
+			for (int a = 0; a < newArray.length; a++) {
+				newArray[a] = name;
+			}
+			
+	return newArray;
 		
 	}
 	
+	public static char[][] createGrid(int rows, int columns, char symbol)  {
+		char[][] newGrid = new char[rows][columns];
+			for (int row = 0; row < rows; row++){
+				for (int col = 0; col < columns; col++) {
+				newGrid[row][col] = symbol;
+				}
+			}
+	return newGrid;
+		
+	}
 	
-
+	public static String gridAsString(char [][]grid) {
+		
+		String thisGridAsString ="";
+		
+		for (int box = 0; box < (grid[0].length*2)+1; box++) {
+			thisGridAsString +="-";
+		}
+		thisGridAsString +="\n";
+		
+		for (int row = 0; row < grid.length; row++) {
+			thisGridAsString += "|";
+			for (int col = 0; col < grid[row].length; col++) {
+			thisGridAsString += grid[row][col];
+			thisGridAsString += "|";
+			}
+			thisGridAsString += "\n";
+		}
+		
+		for (int box = 0; box < (grid[0].length*2)+1; box++) {
+			thisGridAsString +="-";
+		}
+		thisGridAsString +="\n";
+		
+	return thisGridAsString;
+	}
+	
+	
 }
