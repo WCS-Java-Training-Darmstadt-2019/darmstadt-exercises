42b5367f8385940b825976567a0904ad91208406
Robert0816 <duschek@gmx.net>
2020-03-10 14:48:39 +0100

    Zeit leider abgelaufen.

--- checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/conditional/StarTrek.java
+++ checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/conditional/StarTrek.java
@@ -7,19 +7,44 @@ import java.io.InputStreamReader;
 public class StarTrek {
 	
 	public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(
+        
+		for(int i=0;i<10;i++) {
+		BufferedReader reader = new BufferedReader(
               new InputStreamReader(System.in));
  
         System.out.println("Welchen Teil von Startrek möchten sie schauen? Bitte geben sie die Nr ein.");
         
         String eingabe = reader.readLine();
         int filmNo = Integer.parseInt(eingabe);//TODO;
+       
         
-        System.out.println("FilmNo:" + filmNo);
+        
+        if(filmNo==1 || filmNo==3 || filmNo == 5 || filmNo ==7) {
+
+        	System.out.println("FilmNo:" + filmNo);
+        	System.out.println(" " +"Ich würde den Film nicht empfehlen");  
+        	}
+           	else if (filmNo==2) {
+           		System.out.println("Der Zorn des Khan");
+           		break;
+        	}
+           	else if (filmNo==4) {
+           		System.out.println("Zurück in die Gegenwart");
+           		break;
+        	}
+           	else if (filmNo==6) {
+           		System.out.println("ZDas unentdeckte Land");
+           		break;
+        	}
+           	else if (filmNo==8) {
+           		System.out.println("Der erste Kontakt");
+           		break;
+        	}
+		}
+        	
 	}
-	
-	
+}
         
         
 
-}
+
--- checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/vars/VarsMain.java
+++ checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/vars/VarsMain.java
@@ -5,6 +5,44 @@ public class VarsMain {
 	
 	public static void main(String[] args) {
 		
+		String name="david";
+		int age;
+		
+		age = 2020 - 1974;
+		
+		
+//		System.out.println(name);
+		System.out.println(name + " " + "ist" + " " + age + " " + "alt.");
+	
+//		calculateAge(1974, 2020);
+		calculateAge(1974);
+		isOld(30);
+
 	}
+	
+//	static void calculateAge(int year, int currentDate) { 
+	static void calculateAge(int year) {
+		
+		int age;
+		int currentDate=2020;
+		
+		age = currentDate - year;
+		System.out.println(age);
+		
+	}
+	
+	static void isOld(int age) {
+		
+		
+		
+		if(age>0 && age<=30)
+			System.out.println("wahr");
+		else 
+			System.out.println("falsch");
+		
+		
+	}
+	
+	
 
 }
