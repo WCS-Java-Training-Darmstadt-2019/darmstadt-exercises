f39167f65681ed108ecbea6aa403b018e6d4bcc5
Denis Kutlovci <deniskutlovci@hotmail.com>
2020-03-10 16:14:34 +0100

    Hat spaß gemacht, eine stunde ging schnell rum, aber alles ohne hilfe/internet gemacht

--- checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/conditional/StarTrek.java
+++ checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/conditional/StarTrek.java
@@ -13,13 +13,23 @@ public class StarTrek {
         System.out.println("Welchen Teil von Startrek möchten sie schauen? Bitte geben sie die Nr ein.");
         
         String eingabe = reader.readLine();
-        int filmNo = Integer.parseInt(eingabe);//TODO;
+        int filmNo = Integer.parseInt(eingabe);
         
+        if (filmNo > 2) {
+        	System.out.println("Der Film ist gut");	
+        	
+        }else {
+			while (filmNo < 2) {
+        		String eingabe1 = reader.readLine();
+                int filmNo1 = Integer.parseInt(eingabe1);
+                System.out.println("Der Film ist schlecht, versuchen Sie nochmal");
+        	}
+      }
         System.out.println("FilmNo:" + filmNo);
 	}
 	
-	
+	// LEIDER KAM ICH NICHT WEITER WEIL EINE STUNDE RUM IST, 
+	//ABER ICH GLAUBE HÄTTE ICH MEHR ZEIT; HÄTTE ICH ES HIN BEKOMMEN
         
-        
-
+       
 }
--- checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/vars/VarsMain.java
+++ checkpoint_java_basics/src/main/java/com/wcs/java/basics/exercises/vars/VarsMain.java
@@ -3,8 +3,27 @@ package com.wcs.java.basics.exercises.vars;
 
 public class VarsMain {
 	
+	static int calculateAge(int age) {
+		return age;
+	}
+	
+	static void isOld(int age) {
+		if (age <= 30) {
+			System.out.println("Zu Jung");
+		}else {
+			System.out.println("Zu alt");
+		}
+	}
+	
 	public static void main(String[] args) {
 		
+		String name ="David";
+		int age = 2020-1976;
+		System.out.println(name + " ist " + age + " Jahre alt.");
+		
+		calculateAge(age);
+		System.out.println(age);
+		
+		isOld(age);
 	}
-
 }
